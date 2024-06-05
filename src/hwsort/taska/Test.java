3d79d61131047334e25892d2f384aa818dd55f8a
bishnu <bishnup212@gmail.com>
2024-06-05 14:58:09 -0500

    task d uploaded need to check

--- src/hwsort/taska/Test.java
+++ src/hwsort/taska/Test.java
@@ -6,12 +6,12 @@ public class Test {
     public static void main(String[] args) {
         DeptEmployee[] data = DeptEmployeeData.getDeptData();
 
-        System.out.println("Before sorting with respect to name:");
+        System.out.println("Before sorting with respect to name in natural order:");
         System.out.println(Arrays.toString(data));
 
         Arrays.sort(data, new NameComparator());
 
-        System.out.println("After sorting with respect to name:");
+        System.out.println("After sorting with respect to name in natural order:");
         System.out.println(Arrays.toString(data));
 
     }
